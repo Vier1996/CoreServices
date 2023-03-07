fc281d980cfa71d0c0ebd569c1bf1fb7118cf379
apAbsoluteZero <hsF77S6y5BwzVrSzntdJ>
2023-03-07 18:56:57 +0400

    feat: add resizeCallback

--- Packages/com.alexplay.core/Runtime/UI/UIResizer.cs
+++ Packages/com.alexplay.core/Runtime/UI/UIResizer.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,13 +11,13 @@ namespace ACS.Core.UI
         private const float BaseCanvasWidth = 720f;
         private const float BaseScreenRatio = 16 / 9f;
         
-        public void SetupCanvas()
+        public void SetupCanvas(Action onComplete = null)
         {
             GetComponent<CanvasScaler>().referenceResolution = new Vector2(
                 BaseCanvasWidth,
                 BaseCanvasHeight * Mathf.Max(1f, Screen.height / (float) Screen.width / BaseScreenRatio));
 
-            transform.DOScale(0.01f, 0);
+            transform.DOScale(0.01f, 0).OnComplete(() => onComplete?.Invoke());
         }
     }
 }
