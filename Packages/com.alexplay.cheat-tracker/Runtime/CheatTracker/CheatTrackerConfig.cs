e08c16b5b90d67870737e2cb17c3f0b34e30bba5
apAbsoluteZero <hsF77S6y5BwzVrSzntdJ>
2023-04-25 03:25:42 +0400

    add: some files

---
+++ Packages/com.alexplay.cheat-tracker/Runtime/CheatTracker/CheatTrackerConfig.cs
@@ -0,0 +1,15 @@
+﻿using Config;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ACS.Cheat.CheatTracker
+{
+    public class CheatTrackerConfig : ServiceConfigBase
+    {
+        [ReadOnly]
+        [HideInInspector]
+        public string PackageURL = "";
+        
+        [Button] private void UpdatePackage() => UpdatePackage(PackageURL);
+    }
+}
---
+++ Packages/com.alexplay.cheat-tracker/Runtime/CheatTracker/Utilities/EnsureServiceInspectorDefine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+
+namespace ACS.Cheat.CheatTracker.Utilities
+{
+    public class EnsureServiceInspectorDefine
+    {
+        private static readonly string[] DEFINES = new string[] { "COM_ALEXPLAY_NET_CHEAT_TRACKER" };
+
+        [InitializeOnLoadMethod]
+        private static void EnsureScriptingDefineSymbol()
+        {
+            var currentTarget = EditorUserBuildSettings.selectedBuildTargetGroup;
+
+            if (currentTarget == BuildTargetGroup.Unknown)
+            {
+                return;
+            }
+
+            var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(currentTarget).Trim();
+            var defines = definesString.Split(';');
+            
+            bool changed = false;
+
+            foreach (var define in DEFINES)
+            {
+                if (defines.Contains(define) == false)
+                {
+                    if (definesString.EndsWith(";", StringComparison.InvariantCulture) == false)
+                    {
+                        definesString += ";";
+                    }
+
+                    definesString += define;
+                    changed = true;
+                }
+            }
+
+            if (changed)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(currentTarget, definesString);
+            }
+        }
+    }
+}
