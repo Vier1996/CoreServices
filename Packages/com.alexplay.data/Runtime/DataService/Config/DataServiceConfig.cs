35daf7549a1b776e677e925f2110a0b8ef924306
apAbsoluteZero <hsF77S6y5BwzVrSzntdJ>
2024-02-08 11:29:20 +0300

    ref: small refactor

--- Packages/com.alexplay.data/Runtime/DataService/Config/DataServiceConfig.cs
+++ Packages/com.alexplay.data/Runtime/DataService/Config/DataServiceConfig.cs
@@ -25,7 +25,7 @@ namespace ACS.Data.DataService.Config
         
 #if UNITY_EDITOR
         [ShowIf("@_isEnabled == true"), GUIColor(0.5f, 1, 1), PropertyOrder(-1)]
-        [Button] private void UpdateActiveDialogs()
+        [Button] private void UpdateActiveModels()
         {
             Type dialogViewType = typeof(ProgressModel);
             Assembly assembly = AppDomain.CurrentDomain.GetAssemblies().First(atr => atr.GetName().Name.Equals(_sharpAssemblyName));
@@ -54,53 +54,5 @@ namespace ACS.Data.DataService.Config
 
         [Button] private void UpdatePackage() => UpdatePackage(PackageURL);
 #endif
-    }
-    
-    [Serializable]
-    public struct ModelsInfo : IEquatable<ModelsInfo>
-    {
-        [ReadOnly] public string Name;
-        [ReadOnly] public string TypeFullName;
-        [ReadOnly] public string AssemblyName;
-     
-        public bool LockErrasing;
-        
-        public ModelsInfo(Type type)
-        {
-            Name = type.Name;
-            TypeFullName = type.FullName;
-            AssemblyName = type.Assembly.GetName().Name;
-            
-            LockErrasing = false;
-        }
-
-        public Type GetModelType()
-        {
-            Assembly assembly = Assembly.Load(AssemblyName);
-            
-            if (assembly == null)
-                Debug.Log($"Assembly '{AssemblyName}' not found");
-            
-            Type agentType = assembly.GetType(TypeFullName);
-            
-            if (agentType == null)    
-                Debug.Log($"Type with full name '{TypeFullName}' doesn't exist within {AssemblyName}");
-            
-            return agentType;
-
-        }
-
-        public bool Equals(ModelsInfo other) => 
-            TypeFullName == other.TypeFullName && AssemblyName == other.AssemblyName;
-
-        public override bool Equals(object obj) => obj is ModelsInfo other && Equals(other);
-
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                return ((TypeFullName != null ? TypeFullName.GetHashCode() : 0) * 397) ^ (AssemblyName != null ? AssemblyName.GetHashCode() : 0);
-            }
-        }
     }
 }
---
+++ Packages/com.alexplay.data/Runtime/DataService/Config/ModelsInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ACS.Data.DataService.Config
+{
+    [Serializable]
+    public struct ModelsInfo : IEquatable<ModelsInfo>
+    {
+        [ReadOnly] public string Name;
+        [ReadOnly] public string TypeFullName;
+        [ReadOnly] public string AssemblyName;
+     
+        public bool LockErrasing;
+        
+        public ModelsInfo(Type type)
+        {
+            Name = type.Name;
+            TypeFullName = type.FullName;
+            AssemblyName = type.Assembly.GetName().Name;
+            
+            LockErrasing = false;
+        }
+
+        public Type GetModelType()
+        {
+            Assembly assembly = Assembly.Load(AssemblyName);
+            
+            if (assembly == null)
+                Debug.Log($"Assembly '{AssemblyName}' not found");
+            
+            Type agentType = assembly.GetType(TypeFullName);
+            
+            if (agentType == null)    
+                Debug.Log($"Type with full name '{TypeFullName}' doesn't exist within {AssemblyName}");
+            
+            return agentType;
+
+        }
+
+        public bool Equals(ModelsInfo other) => 
+            TypeFullName == other.TypeFullName && AssemblyName == other.AssemblyName;
+
+        public override bool Equals(object obj) => obj is ModelsInfo other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((TypeFullName != null ? TypeFullName.GetHashCode() : 0) * 397) ^ (AssemblyName != null ? AssemblyName.GetHashCode() : 0);
+            }
+        }
+    }
+}
