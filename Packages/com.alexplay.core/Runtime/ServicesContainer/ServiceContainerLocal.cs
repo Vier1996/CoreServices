2d6d3362e93e766cbf897813cd3f2da152587d0d
mrVierPC <maxvier.1996@gmail.com>
2024-02-25 23:46:24 +0300

    rem: remove message

--- Packages/com.alexplay.core/Runtime/ServicesContainer/ServiceContainerLocal.cs
+++ Packages/com.alexplay.core/Runtime/ServicesContainer/ServiceContainerLocal.cs
@@ -1,11 +1,7 @@
-using Sirenix.OdinInspector;
-
 namespace ACS.Core.ServicesContainer
 {
     public class ServiceContainerLocal : Bootstrapper
     {
-        [ReadOnly, ShowInInspector] private readonly string _alert = "Используйте как родительский класс, где каждый наследник представляет бутстрап своей сцены";
-        
         protected override void Bootstrap() => Container.AsScene();
     }
 }
