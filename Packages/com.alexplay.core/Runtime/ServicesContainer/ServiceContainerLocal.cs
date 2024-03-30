bb6c29a71906f8077c05cb2f447cf9bda1d28e57
apAbsoluteZero <hsF77S6y5BwzVrSzntdJ>
2024-03-30 17:53:56 +0300

    fix: fix execution order in sContainer

--- Packages/com.alexplay.core/Runtime/ServicesContainer/ServiceContainerLocal.cs
+++ Packages/com.alexplay.core/Runtime/ServicesContainer/ServiceContainerLocal.cs
@@ -1,5 +1,8 @@
+using UnityEngine;
+
 namespace ACS.Core.ServicesContainer
 {
+    [DefaultExecutionOrder(-11000)]
     public class ServiceContainerLocal : Bootstrapper
     {
         protected override void Bootstrap() => Container.AsScene();
